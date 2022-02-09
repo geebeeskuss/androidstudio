a0a883a7c9125aa9834c18a9285b24fff6b93627
geebeeskuss <murtazalieva.albina.04@mail.ru>
2022-02-09 18:58:51 +0300

    lessons 48

--- lib/screen/tg_settings.dart
+++ lib/screen/tg_settings.dart
@@ -5,10 +5,53 @@ class TelegramSettings  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Telegram settings screen'),
+      appBar: AppBar(title: Text('Telegram settings screen'),
       ),
-
+      backgroundColor: Color(0xFFF1F0F6),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            Icon(Icons.face, size: 200),
+            SizedBox(height: 50),
+            Text('Name Surname',style: TextStyle(fontSize: 40)),
+            SizedBox(height: 50),
+            Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.bookmark,size: 60,color: Colors.blue),
+                      Expanded(child: Text('Saved messages')),
+                      Icon(Icons.arrow_forward_ios,color: Colors.grey),
+                    ],
+                  ),
+                  Divider(color: Colors.grey),
+                  Row(
+                    children: [
+                      Icon(Icons.call,size: 60,color: Colors.green),
+                      Expanded(child: Text('Recent calls')),
+                      Icon(Icons.arrow_forward_ios,color: Colors.grey),
+                    ],
+                  ),
+                  Divider(color: Colors.grey),
+                  Row(
+                    children: [
+                      Icon(Icons.devices_outlined,size: 60,color: Colors.orange),
+                      Expanded(child: Text('Deviced')),
+                      Icon(Icons.arrow_forward_ios,color: Colors.grey),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
+
+
   }
 }
